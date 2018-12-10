5c8ab96504ede3f475d1acdd7678ca8021e2c8bc
eswaribala <Parameswaribala@gmail.com>
2018-12-10 17:17:28 +0530

    Add files via upload

---
+++ LoginSteps.cs
@@ -0,0 +1,38 @@
+﻿using DaimlerProject;
+using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace BDD_DaimlerDemo
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"the user has entered '(.*)' in the Name field")]
+        public void GivenTheUserHasEnteredInTheNameField(string p0)
+        {
+            ScenarioContext.Current.Add("Name", p0);
+        }
+        
+        [Given(@"the user has not entered Name filed value")]
+        public void GivenTheUserHasNotEnteredNameFiledValue()
+        {
+            ScenarioContext.Current.Add("Name", "");
+        }
+        
+        [When(@"I Validate the field")]
+        public void WhenIValidateTheField()
+        {
+                  Member member = new Member();
+            member.Name =(string) ScenarioContext.Current["Name"];
+            ScenarioContext.Current.Add("Result", member.Status);
+
+        }
+        
+        [Then(@"I see a message saying Name is '(.*)'")]
+        public void ThenISeeAMessageSayingNameIs(string p0)
+        {
+            Assert.AreEqual(p0, ScenarioContext.Current["Result"]);
+        }
+    }
+}
