e203780db649ee3dc2d34e8acf0211beac2ac2cc
eswaribala <Parameswaribala@gmail.com>
2018-12-10 17:16:52 +0530

    Add files via upload

---
+++ Member.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaimlerProject
+{
+    public class Member
+    {
+        private string name;
+        public string Status { get; set; }
+        public String Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (value.Length > 0)
+                {
+                    this.name = value;
+                    Status = "valid";
+                }
+                else
+                {
+                    this.name = null;
+                    Status = "invalid";
+                }
+            }
+        }
+
+
+
+    }
+}
