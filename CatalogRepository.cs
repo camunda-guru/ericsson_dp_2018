96fe7639889fa8a75e7030a7f4384f27b4b4650f
eswaribala <Parameswaribala@gmail.com>
2018-12-11 17:01:16 +0530

    Add files via upload

---
+++ CatalogRepository.cs
@@ -0,0 +1,60 @@
+﻿using e_shopCatalogServices.EFContext;
+using e_shopCatalogServices.Models;
+using e_shopCatalogServices.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_shopCatalogServices.Repository
+{
+    public class CatalogRepository :IDisposable,ICatalogRepository
+    {
+        private DataContext _context;
+
+
+        public CatalogRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_context != null)
+                {
+                    _context.Dispose();
+                    _context = null;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        public IEnumerable<Catalog> GetaAll()
+        {
+            return _context.Catalogs;
+        }
+
+        public Catalog GetCatalogById(long id)
+        {
+            var query = from Catalog cat in _context.Catalogs.ToList()
+                        where cat.CatalogId == id
+                        select cat;
+            return query.ToList<Catalog>().First();
+        }
+
+        public void SaveCatalog(Catalog catalog)
+        {
+            
+        }
+    }
+}
---
+++ ICatalogRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using e_shopCatalogServices.Models;
+namespace e_shopCatalogServices.Repository
+{
+    interface ICatalogRepository
+     {
+        void SaveCatalog(Catalog catalog);
+        IEnumerable<Catalog> GetaAll();
+        Catalog GetCatalogById(long id);
+
+
+
+      }
+}
