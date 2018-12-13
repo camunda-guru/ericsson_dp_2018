fe575a406fe87ba71cf5ed5272ab96535b4852cc
eswaribala <Parameswaribala@gmail.com>
2018-12-13 16:35:36 +0530

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Consul;
+using e_shopCatalogServices.Configuration;
+using e_shopCatalogServices.EFContext;
+using e_shopCatalogServices.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
-namespace JWTAuthentication
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace e_shopCatalogServices
 {
     public class Startup
     {
@@ -20,22 +34,36 @@ namespace JWTAuthentication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
+            var connection = @"Data Source=DESKTOP-55AGI0I\SQLEXPRESS;Initial Catalog=EcommerceDB;Persist Security Info=True;User ID=sa;Password=vignesh";
+           services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
+            services.AddScoped< ICatalogRepository, CatalogRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            //services.Configure<ConfigData>(Configuration);
+            services.AddSingleton  <Microsoft.Extensions.Hosting.IHostedService, ConsulHostedService > ();
+            services.Configure < ConsulConfig > (Configuration.GetSection("ConsulConfig"));
+            services.AddSingleton < IConsulClient, ConsulClient > (p => new ConsulClient(consulConfig =>
             {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = Configuration["Jwt:Issuer"],
-                    ValidAudience = Configuration["Jwt:Issuer"],
-                    IssuerSigningKey = new SymmetricSecurityKey
-                    (Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-                };
+                var address = Configuration["ConsulConfig:Address"];
+                consulConfig.Address = new Uri(address);
+            }));
+
+            services.AddSingleton<Func<IConsulClient>>(p => () => new ConsulClient(consulConfig =>
+            {
+                var address = Configuration["ConsulConfig:Address"];
+                consulConfig.Address = new Uri(address);
+            }));
+
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
             });
-            services.AddMvc();
+
+                                 
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,8 +73,12 @@ namespace JWTAuthentication
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
-            app.UseAuthentication();
+            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
